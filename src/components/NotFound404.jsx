14a7581012d4b32fe940c9e2bbd44c307df46c84
Andrey <susluk93@mail.ru>
2026-04-02 07:00:39 +0300

    Modified NotFound404

--- src/components/NotFound404.jsx
+++ src/components/NotFound404.jsx
@@ -4,16 +4,18 @@ import img404 from '../images/404.png'
 
 const NotFound = () => {
   return (
-    <div className='container'>
-      <div className='content'>
-        <div>
-            <img src={img404} alt='дед пожимает плечами'/>
-        </div>
-        <h2>Такой страницы нет</h2>
-        <div className='buttonToMain'>
-            <Link to="/">
-                <button>На главную</button>
-            </Link>
+    <div className='main-container'>
+      <div className='container'>
+        <div className='content'>
+          <div>
+              <img src={img404} alt='дед пожимает плечами'/>
+          </div>
+          <h2>Такой страницы нет</h2>
+          <div className='buttonToMain'>
+              <Link to="/">
+                  <button>На главную</button>
+              </Link>
+          </div>
         </div>
       </div>
     </div>
